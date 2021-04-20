5c5d18e4bb47e6d6c2f5887c76a75fe6fcf9f140
Paulo Silva <paulo@pagus.com.br>
2021-04-20 20:37:41 -0300

    quizcard ok

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:devquiz/home/widgets/appbar/app_bar_widget.dart';
 import 'package:devquiz/home/widgets/level_button/level_button_widget.dart';
+import 'package:devquiz/home/widgets/quiz_card/quiz_card_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -14,21 +15,26 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBarWidget(),
       body: Padding(
         padding: const EdgeInsets.only(right: 20, left: 20, top: 16),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child: Column(
           children: [
-            LevelButtonWidget(
-              label: "Fácil",
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                LevelButtonWidget(
+                  label: "Fácil",
+                ),
+                LevelButtonWidget(
+                  label: "Médio",
+                ),
+                LevelButtonWidget(
+                  label: "Difícil",
+                ),
+                LevelButtonWidget(
+                  label: "Perito",
+                ),
+              ],
             ),
-            LevelButtonWidget(
-              label: "Médio",
-            ),
-            LevelButtonWidget(
-              label: "Difícil",
-            ),
-            LevelButtonWidget(
-              label: "Perito",
-            ),
+            QuizCardWidget()
           ],
         ),
       ),

3a007fa4b076e0149d3e9f6988b3e58c9ac66fa5
Paulo Silva <paulo@pagus.com.br>
2021-04-22 19:08:22 -0300

    send data from home to challenge page

--- lib/challenge/challenge_page.dart
+++ lib/challenge/challenge_page.dart
@@ -1,10 +1,17 @@
+import 'package:flutter/material.dart';
+
 import 'package:devquiz/challenge/widgets/next_button/next_button_widget.dart';
 import 'package:devquiz/challenge/widgets/question_indicator/question_indicator_widget.dart';
 import 'package:devquiz/challenge/widgets/quiz/quiz_widget.dart';
-import 'package:devquiz/core/core.dart';
-import 'package:flutter/material.dart';
+import 'package:devquiz/shared/models/question_model.dart';
 
 class ChallengePage extends StatefulWidget {
+  final List<QuestionModel> questions;
+  const ChallengePage({
+    Key? key,
+    required this.questions,
+  }) : super(key: key);
+
   @override
   _ChallengePageState createState() => _ChallengePageState();
 }
@@ -25,7 +32,9 @@ class _ChallengePageState extends State<ChallengePage> {
                   onPressed: () {
                     Navigator.pop(context);
                   }),
-              QuestionIndicatorWidget(),
+              QuestionIndicatorWidget(
+                questions: widget.questions,
+              ),
             ],
           ),
         ),
@@ -33,7 +42,9 @@ class _ChallengePageState extends State<ChallengePage> {
       body: Container(
         child: Padding(
           padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
-          child: QuizWidget(title: "O que o Flutter faz em sua totalidade"),
+          child: QuizWidget(
+            question: widget.questions[0],
+          ),
         ),
       ),
       bottomNavigationBar: SafeArea(
--- lib/challenge/widgets/quiz/quiz_widget.dart
+++ lib/challenge/widgets/quiz/quiz_widget.dart
@@ -1,11 +1,16 @@
-import 'package:devquiz/challenge/widgets/awnser/awnser_widget.dart';
-import 'package:devquiz/core/core.dart';
 import 'package:flutter/material.dart';
 
-class QuizWidget extends StatelessWidget {
-  final String title;
+import 'package:devquiz/challenge/widgets/awnser/awnser_widget.dart';
+import 'package:devquiz/core/core.dart';
+import 'package:devquiz/shared/models/question_model.dart';
 
-  const QuizWidget({Key? key, required this.title}) : super(key: key);
+class QuizWidget extends StatelessWidget {
+  final QuestionModel question;
+
+  const QuizWidget({
+    Key? key,
+    required this.question,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -17,20 +22,16 @@ class QuizWidget extends StatelessWidget {
               bottom: 24,
             ),
             child: Text(
-              this.title,
+              question.title,
               style: AppTextStyles.heading30,
             ),
           ),
-          AwnserWidget(title: "Kit de desenvolvimento de interface de usuário"),
-          AwnserWidget(
-              isRight: true,
-              isSelected: true,
-              title:
-                  "Possibilita a criação de aplicativos compilados nativamente"),
-          AwnserWidget(title: "Acho que é uma marca de café do Himalaia"),
-          AwnserWidget(
-              title:
-                  "Possibilita a criação de desktops que são muito incríveis")
+          ...question.awnsers
+              .map((e) => AwnserWidget(
+                    title: e.title,
+                    isRight: e.isRight,
+                  ))
+              .toList()
         ],
       ),
     );

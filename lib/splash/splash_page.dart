0720f9be4239e889c028c2a491a864666889435d
Paulo Silva <paulo@pagus.com.br>
2021-04-23 08:26:14 -0300

    splash go to home page

--- lib/splash/splash_page.dart
+++ lib/splash/splash_page.dart
@@ -1,10 +1,20 @@
 import 'package:devquiz/core/app_gradients.dart';
 import 'package:devquiz/core/core.dart';
+import 'package:devquiz/home/home_page.dart';
 import 'package:flutter/material.dart';
 
 class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Future.delayed(Duration(seconds: 2)).then(
+      (value) => Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => HomePage(),
+        ),
+      ),
+    );
+
     return Scaffold(
         body: Container(
       decoration: BoxDecoration(
